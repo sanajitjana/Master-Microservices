8418f845fb0039865a7a0d18302ebda7cdef9ae2
Sanajit Jana <sanajit.jana@mplifytech.com>
2024-06-10 19:25:37 +0530

    Add database connection to currecy exchange service

---
+++ currency-exchange-service/src/main/java/com/sanajitjana/microservices/currency_exchange_service/controller/CurrencyExchangeController.java
@@ -0,0 +1,23 @@
+package com.sanajitjana.microservices.currency_exchange_service.controller;
+
+import com.sanajitjana.microservices.currency_exchange_service.model.CurrencyExchange;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+public class CurrencyExchangeController {
+
+    @Autowired
+    private Environment environment;
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public CurrencyExchange getCurrencyExchangeValue(@PathVariable String from, @PathVariable String to){
+        String port=environment.getProperty("local.server.port");
+        return new CurrencyExchange(1000L, "USD", "INR", BigDecimal.valueOf(82), port);
+    }
+}
---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,12 @@
+--Enter Currency Exchange Data
+insert into currency_exchange
+(id,currency_from,currency_to, conversion_multiple, environment)
+values(1001, 'USD', 'INR',82, '');
+
+insert into currency_exchange
+(id,currency_from,currency_to, conversion_multiple, environment)
+values(1002, 'EUR', 'INR',75, '');
+
+insert into currency_exchange
+(id,currency_from,currency_to, conversion_multiple, environment)
+values(1002, 'AUD', 'INR',65, '');
