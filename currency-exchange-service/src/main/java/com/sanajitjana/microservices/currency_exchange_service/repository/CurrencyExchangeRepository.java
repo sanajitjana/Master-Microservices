45bd05a146e3a5d718037d1bd0f6c4cb21b89e9d
Sanajit Jana <sanajit.jana@mplifytech.com>
2024-06-11 16:59:35 +0530

    Add exception handling

---
+++ currency-exchange-service/src/main/java/com/sanajitjana/microservices/currency_exchange_service/repository/CurrencyExchangeRepository.java
@@ -0,0 +1,11 @@
+package com.sanajitjana.microservices.currency_exchange_service.repository;
+
+import com.sanajitjana.microservices.currency_exchange_service.model.CurrencyExchange;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CurrencyExchangeRepository extends JpaRepository<CurrencyExchange, Long> {
+
+    CurrencyExchange findByFromAndTo(String from, String to);
+}
