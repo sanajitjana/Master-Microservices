fcffb72a5b009f47636b4d324e8d27b0ad41593c
Sanajit Jana <sanajit.jana@mplifytech.com>
2024-06-14 10:32:08 +0530

    remove api-gateway

--- currency-conversion-service/src/main/java/com/sanajitjana/microservices/currency_conversion_service/controller/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/sanajitjana/microservices/currency_conversion_service/controller/CurrencyConversionController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -13,12 +14,13 @@ import java.math.BigDecimal;
 import java.util.HashMap;
 
 @RestController
+@RequestMapping("/currency")
 public class CurrencyConversionController {
 
     @Autowired
     private CurrencyExchangeProxy currencyExchangeProxy;
 
-    @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
+    @GetMapping("/conversion/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversion convert(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
 
         HashMap<String, String> payload = new HashMap<>();
@@ -37,7 +39,7 @@ public class CurrencyConversionController {
         );
     }
 
-    @GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
+    @GetMapping("/conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversion convertUsingFeign(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
 
         CurrencyConversion currencyConversion = currencyExchangeProxy.getExchangeValue(from, to);
--- currency-exchange-service/src/main/java/com/sanajitjana/microservices/currency_exchange_service/controller/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/sanajitjana/microservices/currency_exchange_service/controller/CurrencyExchangeController.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/currency")
 public class CurrencyExchangeController {
 
     @Autowired
@@ -17,7 +19,7 @@ public class CurrencyExchangeController {
     @Autowired
     private Environment environment;
 
-    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    @GetMapping("/exchange/from/{from}/to/{to}")
     public CurrencyExchange getCurrencyExchangeValue(@PathVariable String from, @PathVariable String to) throws Exception {
 
         CurrencyExchange currencyExchange=currencyExchangeRepository.findByFromAndTo(from, to);
