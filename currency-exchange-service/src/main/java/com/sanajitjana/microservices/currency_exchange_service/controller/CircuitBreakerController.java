b03313a6b6e9c29d0ef6fe6275094a5bb34ef2c7
Sanajit Jana <sanajit.jana@mplifytech.com>
2024-06-17 17:29:23 +0530

    Add sprifng stater aop and resilence4j for circuitbreker

---
+++ currency-exchange-service/src/main/java/com/sanajitjana/microservices/currency_exchange_service/controller/CircuitBreakerController.java
@@ -0,0 +1,14 @@
+package com.sanajitjana.microservices.currency_exchange_service.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CircuitBreakerController {
+
+    @GetMapping("sample-api")
+    public String sampleApi() {
+        return "Sample API";
+    }
+
+}
